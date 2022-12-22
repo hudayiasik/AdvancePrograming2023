b65e8957bcb55dd180431a165370c7e907be3c7c
Hudayi <asikhudayi@gmail.com>
2022-12-22 13:08:00 +0300

    project

---
+++ Project/EnemyController.js
@@ -0,0 +1,39 @@
+import Enemy from "./Enemy.js";
+export default class EnemyController {
+
+    enemyMap = [
+        [0, 1, 1, 1, 0, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    ];
+    enemyRows = [];
+    constructor(canvas) {
+        this.canvas = canvas
+    }
+
+    draw(ctx) {
+
+        this.drawEnemies(ctx)
+
+    }
+    drawEnemies(ctx) {
+        this.enemyRows.flat().forEach((enemy) => {
+            enemy.draw(ctx)
+        });
+    }
+    createEnemies() {
+        this.enemyMap.forEach((row, rowIndex) => {
+            this.enemyRows[rowIndex] = [];
+            row.forEach((enemyNubmer, enemyIndex) => {
+                if (enemyNubmer > 0) {
+                    this.enemyRows[rowIndex].push(
+                        new Enemy(enemyIndex * 50, rowIndex * 35, enemyNubmer)
+                    );
+                }
+            });
+        });
+    }
+}
